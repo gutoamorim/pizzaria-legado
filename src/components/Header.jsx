88ee1aadc74be8a6e59717ad18f578a3bd7321bc
Cesar Augusto <gutoamorim@outlook.com>
2024-01-17 20:25:37 -0300

    Adicionando link a logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,9 @@ export default function Header() {
   return (
     <header className="h-20 p-2 bg-red-500">
       <div className="container m-auto flex items-center justify-between">
-        <img src={logo} alt="Logomarca" className="max-h-16" />
+        <Link to="/">
+          <img src={logo} alt="Logomarca" className="max-h-16" />
+        </Link>
         <nav className="flex items-center gap-4 text-white font-medium">
           <Link to="/">Home</Link>
           <Link to="/pizzas">Pizzas</Link>
